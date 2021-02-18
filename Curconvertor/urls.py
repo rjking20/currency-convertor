5418a2adccacdbc2323db9d8aa74317991799c1e
Rj King <42872016+rjking20@users.noreply.github.com>
2021-02-18 15:52:15 +0530

    Update urls.py

--- Curconvertor/urls.py
+++ Curconvertor/urls.py
@@ -2,7 +2,7 @@
 from django.urls import path
 from .views import home,convertor,buttonAction
 urlpatterns = [
-    #path('',home,name='home'),
+  
     path('',convertor,name='currency'),
     path('Action/',buttonAction,name='buttonAction'),
 ]
